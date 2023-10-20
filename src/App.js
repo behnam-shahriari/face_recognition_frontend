e9cfcf8707e428cadd525bb87556ff422468f431
Behnam <shahriaribehnam@gmail.com>
2023-10-20 10:52:55 +0300

    Choosing random particle

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Logo from "./components/Logo/Logo";
@@ -6,6 +7,7 @@ import Rank from "./components/Rank/Rank";
 import ParticlesBg from "particles-bg";
 
 function App() {
+  const [particleType, setParticleType] = useState("");
   const particleOptions = [
     "color",
     "circle",
@@ -16,15 +18,19 @@ function App() {
     "fountain",
   ];
 
+  useEffect(() => {
+    setTimeout(getParticleType, 15000);
+  }, [particleType]);
+
+  const getParticleType = () => {
+    setParticleType(
+      particleOptions[Math.floor(Math.random() * particleOptions.length)]
+    );
+  };
+
   return (
     <div className="App">
-      <ParticlesBg
-        className="particles"
-        type={
-          particleOptions[Math.floor(Math.random() * particleOptions.length)]
-        }
-        bg={true}
-      />
+      <ParticlesBg className="particles" type={particleType} bg={true} />
       <Navigation />
       <Logo />
       <Rank />
