e68cba858b48079393b189702f216db2364ef60a
Behnam <shahriaribehnam@gmail.com>
2023-11-25 13:27:21 +0200

    Rank Component has been modified

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,8 +1,10 @@
 const Rank = () => {
   return (
     <div>
-      <div className="white f3">{"Behnam, Your current rank is..."}</div>
-      <div className="white f1">{"#6"}</div>
+      <div className="white f3">
+        {`Behnam, You have submitted ${6} photos.`}
+        {/* <span className="white f1">{"#6"}</span> */}
+      </div>
     </div>
   );
 };
