a71a8edb1045dff01906238e026744cb1ff47a6d
Behnam <shahriaribehnam@gmail.com>
2023-12-03 15:26:35 +0200

    React env

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "tachyons";
-require("dotenv").config();
-
+// Import dotenv and configure
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <App />
