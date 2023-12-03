15bac70379cf4155238e9952970fe922f15b63c2
Behnam <shahriaribehnam@gmail.com>
2023-12-03 09:54:07 +0200

    Clarifai in Backend

--- src/App.js
+++ src/App.js
@@ -114,17 +114,7 @@ class App extends Component {
       boxClaculated: false,
       generalImageOptions: [],
     });
-    ///////////////////////////////////////////////////////////////////////////////////////////////////
-    // In this section, we set the user authentication, user and app ID, model details, and the URL
-    // of the image we want as an input. Change these strings to run your own example.
-    //////////////////////////////////////////////////////////////////////////////////////////////////
 
-    // Your PAT (Personal Access Token) can be found in the portal under Authentification
-    const PAT = "7a642105b2214d968da39d8e956b3914";
-    // Specify the correct user_id/app_id pairings
-    // Since you're making inferences outside your app's scope
-    const USER_ID = "clarifai";
-    const APP_ID = "main";
     // Change these to whatever model and image URL you want to use
     const MODEL_ID =
       typeId === "fr"
@@ -140,47 +130,17 @@ class App extends Component {
         : "";
     const IMAGE_URL = input;
 
-    ///////////////////////////////////////////////////////////////////////////////////
-    // YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
-    ///////////////////////////////////////////////////////////////////////////////////
-
-    const raw = JSON.stringify({
-      user_app_id: {
-        user_id: USER_ID,
-        app_id: APP_ID,
-      },
-      inputs: [
-        {
-          data: {
-            image: {
-              url: IMAGE_URL,
-            },
-          },
-        },
-      ],
-    });
-
-    const requestOptions = {
+    fetch("http://localhost:3000/imageUrl", {
       method: "POST",
       headers: {
-        Accept: "application/json",
-        Authorization: "Key " + PAT,
+        "Content-Type": "application/json",
       },
-      body: raw,
-    };
-
-    // NOTE: MODEL_VERSION_ID is optional, you can also call prediction with the MODEL_ID only
-    // https://api.clarifai.com/v2/models/{YOUR_MODEL_ID}/outputs
-    // this will default to the latest version_id
-
-    fetch(
-      "https://api.clarifai.com/v2/models/" +
-        MODEL_ID +
-        "/versions/" +
-        MODEL_VERSION_ID +
-        "/outputs",
-      requestOptions
-    )
+      body: JSON.stringify({
+        modelId: MODEL_ID,
+        modelVersionId: MODEL_VERSION_ID,
+        imageUrl: IMAGE_URL,
+      }),
+    })
       .then((response) => response.json())
       .then((result) => {
         if (result) {
